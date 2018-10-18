85ca4feca0320852043e4e5214bc4bab5454a10c
Xuanyu Li <li.xua@husky.neu.edu>
2018-10-18 12:36:12 -0400

    XuanyuLi

--- city_parts.js
+++ city_parts.js
@@ -1,16 +1,16 @@
 "use strict"
 /*--------------------------------------------------Array----------------------------------------------------*/
- function rectArray(hDis,vDis,hNum,vNum,d,x,y,func, pos) {
+ function rectArray(hDis,vDis,hNum,vNum,d,x,y,func, w, h, pos) {
     if (vNum == 0){
         var buildingWidth = hDis * hNum;
-        func(d, pos + buildingWidth * 0.15, y + vDis);
-        func(d, pos + buildingWidth * 0.73, y + vDis);
+        func(d, pos + buildingWidth * 0.15, y + vDis, w, h);
+        func(d, pos + buildingWidth * 0.73, y + vDis, w, h);
     } else {
         let i = 0;
         let j = 0;
         while(i < hNum){
             while(j < vNum){
-                func(d,x + i * hDis,y - j * vDis);
+                func(d,x + i * hDis,y - j * vDis,w,h);
                 j++;
             }
         j = 0;
@@ -23,7 +23,7 @@
 function ground(d){
     let innerDraw = d.nested();
     // draw the base
-    innerDraw.rect(1280,100).move(0,800).fill('grey').stroke('black');
+    innerDraw.rect(d.width(),100).move(0,800).fill('grey').stroke('black');
     rectArray(100,0,15,1,d,0,0,roadStrips);
 }
 
@@ -39,21 +39,21 @@ function roadStrips (d,x,y){
 }
 
 /*---------------------------------------------Building Bases-----------------------------------------------*/
-function baseArray(d,x,numOfFloor){
-    rectArray(0,wallHeight,1,numOfFloor,d,x,800 - wallHeight,buildingBase);
+function baseArray(d,x,numOfFloor, width, height){
+    rectArray(0,height,1,numOfFloor,d,x,800 - height,buildingBase, width, height);
 }
 
-function buildingBase(d,x,y){
+function buildingBase(d,x,y, width, height){
     let innerDraw = d.nested();
-    innerDraw.rect(wallWidth,wallHeight).move(x,y).fill('orange').stroke({width: 1.5, color: 'black'});
-    innerDraw.rect(wallWidth,10).move(x,y).fill('grey').stroke({width: 1.5, color: 'black'});
+    innerDraw.rect(width,height).move(x,y).fill('orange').stroke({width: 1.5, color: 'black'});
+    innerDraw.rect(width,10).move(x,y).fill('grey').stroke({width: 1.5, color: 'black'});
     // create the bricks
-    bricks(d,x,y,wallWidth,wallHeight);
+    bricks(d,x,y,width, height);
 }
 
 function bricks(d,x,y,w,h){
-    rectArray(10,10,w/10,h/10,d,x,y + wallHeight - 5, wallStripes);
-    rectArray(10,10,w/10 - 1,h/10,d,x + 5,y + wallHeight - 10, wallStripes);
+    rectArray(10,10,w/10,h/10,d,x,y + h - 5, wallStripes, w, h);
+    rectArray(10,10,w/10 - 1,h/10,d,x + 5,y + h - 10, wallStripes);
 }
 
 function wallStripes(d,x,y){
@@ -61,8 +61,8 @@ function wallStripes(d,x,y){
     innerDraw.rect(10,5).move(x,y).fill('none').stroke({color: 'black', opacity: 0.05});
 }
 /*--------------------------------------------Building Windows-----------------------------------------------*/
-function windowAArray(d,x,floorNum,winNum){
-    rectArray(wallWidth / winNum,wallHeight,winNum,floorNum - 1,d,x + 11 * (6-winNum) + (5-winNum),720-wallHeight,windowA, x);
+function windowAArray(d,x,floorNum,winNum,w,h){
+    rectArray(w / winNum,h,winNum,floorNum - 1,d,x + 11 * (6-winNum) + (5-winNum),720-h,windowA, w,d, x);
 }
 
 function windowA(d,x,y){
@@ -75,8 +75,8 @@ function windowA(d,x,y){
     innerDraw.rect(40,2).move(x, y + 40).fill('white');
 }
 
-function windowBArray(d,x,floorNum,winNum){
-    rectArray(wallWidth / winNum,wallHeight,winNum,floorNum - 1,d,x + 7 * (6-winNum) + (5-winNum),760-wallHeight,windowB, x);
+function windowBArray(d,x,floorNum,winNum, w, h){
+    rectArray(w / winNum,h,winNum,floorNum - 1,d,x + 7 * (6-winNum) + (5-winNum),760-h,windowB, w,d, x);
 }
 
 function windowB(d,x,y){
@@ -88,8 +88,8 @@ function windowB(d,x,y){
     innerDraw.rect(2,46).center(x + 16.5,y + 7).fill('white');
 }
 
-function windowCArray(d,x,floorNum,winNum){
-    rectArray(wallWidth / winNum,wallHeight,winNum,floorNum - 1,d,x + 11 * (6-winNum) + (5-winNum),720-wallHeight,windowC, x);
+function windowCArray(d,x,floorNum,winNum, w, h){
+    rectArray(w / winNum,h,winNum,floorNum - 1,d,x + 11 * (6-winNum) + (5-winNum),720-h,windowC, w, h, x);
 }
 
 function windowC(d,x,y){
@@ -99,8 +99,8 @@ function windowC(d,x,y){
     innerDraw.rect(2,60).move(x + 18.5,y).fill('white');
 }
 
-function windowDArray(d,x,floorNum,winNum){
-    rectArray(wallWidth / winNum,wallHeight,winNum,floorNum - 1,d,x + 7 * (6-winNum) + (5-winNum),740-wallHeight,windowD, x);
+function windowDArray(d,x,floorNum,winNum, w, h){
+    rectArray(w / winNum,h,winNum,floorNum - 1,d,x + 7 * (6-winNum) + (5-winNum),740-h,windowD, w,h, x);
 }
 
 function windowD(d,x,y){
@@ -111,8 +111,8 @@ function windowD(d,x,y){
     innerDraw.rect(2,30).move(x + 13.5,y).fill('white');
 }
 
-function windowEArray(d,x,floorNum,winNum){
-    rectArray(wallWidth / winNum,wallHeight,winNum,floorNum - 1,d,x + 7 * (6-winNum) + (5-winNum),720-wallHeight,windowE, x);
+function windowEArray(d,x,floorNum,winNum,w, h){
+    rectArray(w / winNum,h,winNum,floorNum - 1,d,x + 7 * (6-winNum) + (5-winNum),720-h,windowE,w,h, x);
 }
 
 function windowE(d,x,y){
@@ -120,46 +120,46 @@ function windowE(d,x,y){
     innerDraw.rect(30,20).move(x,y).fill('lightblue').stroke({width: 2, color: 'white'});
 }
 /*--------------------------------------------Building Roofs------------------------------------------------*/
-function roofA(d,x,y){
+function roofA(d,x,y, w, h){
     let innerDraw = d.nested();
-    innerDraw.rect(wallWidth-2,78).move(x + 1,y).fill('brown').stroke({width: 4, color: 'black'});
-    bricks(d,x,y - 20,wallWidth,80);
-    rectArray(20,0,wallWidth / 20 - 1,1,d,x + 12.5,y + 15,roofStripe);
+    innerDraw.rect(w-2,78).move(x + 1,y).fill('brown').stroke({width: 4, color: 'black'});
+    bricks(d,x,y,w,80);
+    rectArray(20,0,w / 20 - 1,1,d,x + 12.5,y + 15,roofStripe);
 }
 
-function roofB(d,x,y){
+function roofB(d,x,y, w){
     let innerDraw = d.nested();
     innerDraw.polygon([
         [3,60],
-        [wallWidth - 3,60],
-        [wallWidth - 40,0],
+        [w - 3,60],
+        [w - 40,0],
         [40,0]
     ]).move(x + 3,y).fill('brown').stroke({width: 4, color: 'black'});
 }
 
-function roofC(d,x,y){
+function roofC(d,x,y, w){
     let innerDraw = d.nested();
     innerDraw.polygon([
         [3,69],
-        [wallWidth - 3,69],
-        [wallWidth / 2,0]
+        [w - 3,69],
+        [w / 2,0]
     ]).move(x + 3,y).fill('brown').stroke({width: 4, color: 'black'});
 }
 
-function roofD(d,x,y){
+function roofD(d,x,y, w){
     let innerDraw = d.nested();
     innerDraw.polygon([
         [40,59],
-        [wallWidth - 40,59],
-        [wallWidth / 2,0]
+        [w - 40,59],
+        [w / 2,0]
     ]).move(x + 40,y).fill('brown').stroke({width: 4, color: 'black'});
 }
 
-function roofE(d,x,y){
+function roofE(d,x,y, w){
     let innerDraw = d.nested();
-    innerDraw.rect(wallWidth-2,78).move(x + 1,y).fill('brown').stroke({width: 4, color: 'black'});
-    bricks(d,x,y - 40,wallWidth,80);
-    rectArray(20,0,wallWidth / 20 - 1,1,d,x + 10,y + 15,roofStripe);
+    innerDraw.rect(w-2,78).move(x + 1,y).fill('brown').stroke({width: 4, color: 'black'});
+    bricks(d,x,y,w,80);
+    rectArray(20,0,w / 20 - 1,1,d,x + 10,y + 15,roofStripe);
 }
 
 function roofStripe (d,x,y){
@@ -170,32 +170,32 @@ function roofStripe (d,x,y){
 }
 /*--------------------------------------------Building Doors------------------------------------------------*/
 
-function doorA(d,x){
+function doorA(d,x, w){
     let innerDraw = d.nested();
-    innerDraw.rect(40,70).move(x + wallWidth / 2 - 20,730).fill('grey').stroke({width: 2, color: 'black'});
-    innerDraw.circle(6).center(x + wallWidth / 2 - 15,765).fill('green').stroke('black');
+    innerDraw.rect(40,70).move(x + w / 2 - 20,730).fill('grey').stroke({width: 2, color: 'black'});
+    innerDraw.circle(6).center(x + w / 2 - 15,765).fill('green').stroke('black');
 }
 
-function doorB(d,x){
+function doorB(d,x,w){
     let innerDraw = d.nested();
-    innerDraw.rect(60,60).move(x + wallWidth/2 - 30,740).fill('green').stroke({width: 2, color: 'black'});
+    innerDraw.rect(60,60).move(x + w/2 - 30,740).fill('green').stroke({width: 2, color: 'black'});
     // draw door mid stroke
-    innerDraw.rect(2,60).move(x + wallWidth/2 - 1 ,740).fill('black');
+    innerDraw.rect(2,60).move(x + w/2 - 1 ,740).fill('black');
     // draw door handle
-    innerDraw.rect(3,6).move(x + wallWidth/ 2 + 3, 768).fill('grey').stroke('black');
-    innerDraw.rect(3,6).move(x + wallWidth/ 2 - 8, 768).fill('grey').stroke('black');
+    innerDraw.rect(3,6).move(x + w/ 2 + 3, 768).fill('grey').stroke('black');
+    innerDraw.rect(3,6).move(x + w/ 2 - 8, 768).fill('grey').stroke('black');
 }
 /*-------------------------------------------------Car---------------------------------------------------------*/
 /*
     Function:       car
-    
+
     Description:    Draws a car
 
-    
+
     Parameters:     d = SVG drawing area
                     x = horizontal coordinate,
                     y = vertical coordinate
-    
+
     Author:         Suifeng Liu
 */
 function car (d,x,y) {
@@ -206,12 +206,12 @@ function car (d,x,y) {
         [220,100],
         [220,50],
         [170,50],
-        [150,0],    
+        [150,0],
         [70,0],
         [50,50],
         [0,50]
                     ]).move(x,y + 475).fill('red').stroke({width: 2});
-    
+
     // Draw the glass
     carparts.polygon([
         [55,50],
@@ -219,7 +219,7 @@ function car (d,x,y) {
         [105,4],
         [105,50]
     ]).move(x + 55,y + 480).fill("#6E8898").stroke({width: 2});
-    
+
     carparts.polygon([
         [63,50],
         [63,4],
--- main.js
+++ main.js
@@ -4,154 +4,56 @@ let numberOfBuildings = 5; // Range 0-5
 let numberOfFloors = 1; // Range 1-5
 let numberOfWindows = 5; // Range 1-5
 
-let wallWidth;
-let wallHeight;
-
-let houseAx = 5;
-let houseBx = 310;
-let houseCx = 535;
-let houseDx = 840;
-let houseEx = 1050;
-
 /*---------------------------------------------Canvas-----------------------------------------------*/
 // Set up drawings
 let canvas = SVG("drawing");
 
 // Set up canvas sizes
-canvas.size(1280,900);
+canvas.size(1500,900);
 
 // draw rectangles around each canvas so you can see them for this demo
 canvas.rect(canvas.width(), canvas.height()).fill("white").stroke("black");
 
 /*---------------------------------------------City------------------------------------------------*/
-ground(canvas);
+
 drawCity(canvas,numberOfFloors,numberOfWindows,numberOfBuildings);
+ground(canvas);
 
-
-
-// Bill
-//let arrayOfHouse = [drawHouseA, drawHouseB, drawHouseC, drawHouseD, drawHouseE];
-//let arrayOfHouseX = [houseAx, houseBx, houseCx, houseDx, houseEx];
 
 function drawCity(d,floorNum,winNum,buildingNum) {
-//   Bill
-//    let idx = 0;
-//    while(idx < buildingNum) {
-//        arrayOfHouse[idx](d, arrayOfHouseX[idx], floorNum, winNum);
-//        idx++;
-//    }
-    
-    if (buildingNum >= 1){
-        drawHouseA(d,houseAx,floorNum,winNum);
-    }
-    
-    if (buildingNum >= 2){
-        drawHouseB(d,houseBx,floorNum,winNum);
-    }
-    
-    if (buildingNum >= 3){
-        drawHouseC(d,houseCx,floorNum,winNum);
-    }
-    
-    if (buildingNum >= 4){
-        drawHouseD(d,houseDx,floorNum,winNum);
-    }
-    
-    if (buildingNum == 5){
-        drawHouseE(d,houseEx,floorNum,winNum);
-    }
-//   if (buildingNum == 0){
-//        drawHouseA(d,houseAx,floorNum,winNum);
-//   } else if (buildingNum != 1) {
-//        drawHouseB(d,houseBx,floorNum,winNum);
-//   } else if (buildingNum != 2) {
-//        drawHouseC(d,houseCx,floorNum,winNum);
-//   } else if (buildingNum != 3) {
-//        drawHouseD(d,houseDx,floorNum,winNum);
-//   } else if (buildingNum != 4) {
-//        drawHouseE(d,houseEx,floorNum,winNum);
-//   } else {
-//       canvas.clear();
-//   }
-    ground(d);
-}
-function drawHouseA(d,x,floorNum,winNum) {
-    wallWidth = 300;
-    wallHeight = 100;
-    // draw the base of houseA
-    baseArray(canvas,houseAx,floorNum);
-    // draw the windows of houseA
-    windowAArray(canvas,houseAx,floorNum,winNum);
-    // draw roof of houseA
-    roofA(d,houseAx,820 - (floorNum + 1) * wallHeight);
-    // draw door
-    doorA(d, houseAx);
+  let houseSepcs = [[300, 100, windowAArray, roofA, 820, doorA],
+                    [220, 80,  windowBArray, roofB, 820, doorB],
+                    [300, 100, windowCArray, roofC, 830, doorA],
+                    [200, 90,  windowDArray, roofD, 830, doorB],
+                    [210, 120, windowEArray, roofE, 840, doorB]];
+   let idx = 0;
+   let currentX = 5;
+   while(idx < buildingNum) {
+       currentX = currentX + drawHouse(d, currentX, floorNum, winNum, houseSepcs[Math.floor(Math.random() * 5)]) + 5;
+       idx++;
+   }
 }
 
-function drawHouseB(d,x,floorNum,winNum){
-    wallHeight = 80;
-    wallWidth = 220;
-    // draw the base of houseB
-    baseArray(canvas,houseBx,floorNum);
-    // draw the windows of houseB
-    windowBArray(canvas,houseBx,floorNum,winNum);
-    // draw the roof of houseB
-    roofB(d,houseBx,820 - (floorNum + 1) * wallHeight);
-    // draw the door of houseB
-    doorB(d,houseBx); 
-}
-
-function drawHouseC(d,x,floorNum,winNum){
-    wallHeight = 110;
-    wallWidth = 300;
-    // draw the base of houseC
-    baseArray(canvas,houseCx,floorNum);
-    // draw the windows of houseC
-    windowCArray(canvas,houseCx,floorNum,winNum);
-    // draw the roof of houseC
-    roofC(d,houseCx,840 - (floorNum + 1) * wallHeight);
-    // draw the door of houseC
-    doorA(d,houseCx);
-}
-
-function drawHouseD(d,x,floorNum,winNum){
-    wallHeight = 90;
-    wallWidth = 200;
-    // draw the base of houseD
-    baseArray(canvas,houseDx,floorNum);
-    // draw the windows of houseD
-    windowDArray(canvas,houseDx,floorNum,winNum);
-    // draw the roof of houseD
-    roofD(d,houseDx,830 - (floorNum + 1) * wallHeight);
-    // draw the door of houseD
-    doorA(d,houseDx);
-}
-
-function drawHouseE(d,x,floorNum,winNum){
-    wallHeight = 120;
-    wallWidth = 210;
-    // draw the base of houseE
-    baseArray(canvas,houseEx,floorNum);
-    // draw the windows of houseE
-    windowEArray(canvas,houseEx,floorNum,winNum);
-    // draw the roof of houseE
-    roofE(d,houseEx,840 - (floorNum + 1) * wallHeight);
-    // draw the door of houseE
-    doorB(d,houseEx);
+function drawHouse(d, x, floorNum, winNum, spec){
+  baseArray(d, x, floorNum, spec[0], spec[1]);
+  spec[2](d, x, floorNum, winNum, spec[0], spec[1]);
+  spec[3](d, x, spec[4] - (floorNum + 1) * spec[1], spec[0]);
+  spec[5](d, x, spec[0]);
+  return spec[0];
 }
 
 /*
     This code does not even set up your SVG canvas, so you need to do that first.
     For this assignment, you don't have to have to have a "default city" that draws
     before the user picks options, as I did in Homework 1E.
-    
+
     Instead, you will need to add at least one line at the end of the formChanged()
     event handler function that calls your city drawing code.
-    
+
     If you are unsure of what this might look like, review Homework 1E to see how I
     did it. There are many other ways to do it besides how I did it, so pick what makes
     the most sense to you.
-    
+
     PLEASE DELETE THIS COMMENT BEFORE TURNING IN YOUR CODE!
 */
 
@@ -174,11 +76,11 @@ function formChanged(event) {
 
     /* All data processed. Start drawing!*/
     console.log("******* LET'S DRAW A CITY! ********");
-    
+
     canvas.clear();
     drawCity(canvas,numberOfFloors,numberOfWindows,numberOfBuildings);
     ground(canvas);
-    
+
     if (Math.random() < 0.75) {
     car(canvas, 0, 230).animate(2500).x(1500);
     car(canvas, 0, 290).animate(2000).x(1500);
